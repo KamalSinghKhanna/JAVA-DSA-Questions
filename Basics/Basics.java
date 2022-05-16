29d3f0c247d34e9d20254b166bcfaee5c4078183
KamalSinghKhanna <kamalsinghkhanna2706@gmail.com>
2022-05-16 11:04:02 +0530

    basics

--- Basics/Basics.java
+++ Basics/Basics.java
@@ -4,13 +4,14 @@ package Basics;
 
 public class Basics {
     public static void main(String[] args) {
-        // Scanner input = new Scanner(System.in);
-        // int a = input.nextInt();
-        // int b = input.nextInt();
-        // int n = input.nextInt();
-        int a = 0;
-        int b  = 1;
-        int n = 7;
+        // fibonacci(0, 1, 7);
+        // System.out.println(occurence(234454, 4));
+        System.out.println(reverse(1234));
+    }
+
+    
+    //fibonacci
+    static void fibonacci(int a, int b, int n){
         System.out.println(a);
         System.out.println(b);
         for (int i = 2; i <= n; i++)
@@ -22,5 +23,30 @@ public class Basics {
 
         }
        
+    }
+    //how many time second number occure in first?
+
+    static int occurence(int num1, int num2) {
+        int count=0;
+        while (num1 > 0) {
+            int rem = num1%10;
+            if (rem == num2) {
+                count++;
+            }
+            num1 = num1/10;
+        }
+        return count;
+    }
+
+// find the reverse of number
+    static int reverse(int num) {
+        int ans = 0;
+        while(num>0) {
+            int rem = num%10;
+            ans = ans*10+rem;
+            num = num/10;
+        }
+        return ans;
+        
     }
 }
